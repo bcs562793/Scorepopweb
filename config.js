4a6807d382b6237740653917387b51f36a0aeffe
bcs562793 <bcs562793@users.noreply.github.com>
2026-03-12 15:07:59 +0000

    deploy: 33672e6239c9c0514a7284cb7f5a40578a61a2ee

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 const SUPABASE_URL = 'https://qyebmwiuvrmfxlgdaarc.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_AL5OMxT8SzcNnxslOt9YSg_3j0qYkL5';
+const PAYMENT_EDGE_URL = "https://qyebmwiuvrmfxlgdaarc.supabase.co/functions/v1";
