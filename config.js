94fbac3484d86ce77b8498bee62645b45a516d22
bcs562793 <79099644+bcs562793@users.noreply.github.com>
2026-03-13 17:31:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = 'BURAYA_URL';
 const SUPABASE_KEY = 'BURAYA_KEY';
-const PAYMENT_EDGE_URL = "https://qyebmwiuvrmfxlgdaarc.supabase.co/functions/v1";
+const PAYMENT_EDGE_URL = "https://qyebmwiuvrmfxlgdaarc.supabase.co/functions/v1/create-shopier-payments";
