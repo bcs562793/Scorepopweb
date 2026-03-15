313fc61116e5cc4319cebcdc64090b46e8611a4a
bcs562793 <bcs562793@users.noreply.github.com>
2026-03-15 13:57:52 +0000

    deploy: 9412ce368c29335b967ce0a116715b4f96c15ed8

--- config.js
+++
@@ -1,3 +0,0 @@
-const SUPABASE_URL = 'https://qyebmwiuvrmfxlgdaarc.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_AL5OMxT8SzcNnxslOt9YSg_3j0qYkL5';
-const PAYMENT_EDGE_URL = "https://qyebmwiuvrmfxlgdaarc.supabase.co/functions/v1/create-shopier-payments";
