75d8cdba9a4bc724a152f0b7949461502337c025
bcs562793 <bcs562793@users.noreply.github.com>
2026-06-30 21:14:39 +0000

    deploy: f45b83054bb4f3590f2014599b27a26b1b884614

--- sitemap-gen.js
+++ sitemap-gen.js
@@ -120,6 +120,7 @@ async function generate() {
     `  <url>\n    <loc>${BASE_URL}/bugun</loc>\n    <lastmod>${fmt(today)}</lastmod>\n    <changefreq>hourly</changefreq>\n    <priority>0.9</priority>\n  </url>`,
     `  <url>\n    <loc>${BASE_URL}/canli</loc>\n    <lastmod>${now}</lastmod>\n    <changefreq>always</changefreq>\n    <priority>0.9</priority>\n  </url>`,
     `  <url>\n    <loc>${BASE_URL}/tahmin</loc>\n    <lastmod>${now}</lastmod>\n    <changefreq>hourly</changefreq>\n    <priority>0.8</priority>\n  </url>`,
+    `  <url>\n    <loc>${BASE_URL}/transferler</loc>\n    <lastmod>${now}</lastmod>\n    <changefreq>hourly</changefreq>\n    <priority>0.8</priority>\n  </url>`,
     `  <url>\n    <loc>${BASE_URL}/wc-2026</loc>\n    <lastmod>${now}</lastmod>\n    <changefreq>weekly</changefreq>\n    <priority>0.7</priority>\n  </url>`,
     ...matches.map(m => {
       const slug    = `${slugify(m.home_team)}-vs-${slugify(m.away_team)}`;
